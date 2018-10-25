ab8f11a6bea0a01f11236bcb7d61d716b03337b1
gokulramanaa <gokul_ramanan@hotmail.com>
2018-10-25 06:58:19 -0400

    gcloud config added

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+#setup parameters fro Google Cloud ML ENginee
+set(name='trainer',
+        version='0.1',
+        packages=find_packages(),
+        description='ml facerecognition',
+        author='gokul',
+        author_email='dadmumbro@gmail.com',
+        licence='MIT',
+        install_requires=[
+            '',
+            '',
+            ],
+        zip_safe=False)
